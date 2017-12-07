528f82ebb3adc92f1fa9da0f1e94f30e47907105
Blake DeBoer <blakedeboer@users.noreply.github.com>
2017-12-07 17:29:30 -0500

    Change four spaces to two spaces

--- tempChanger.js
+++ tempChanger.js
@@ -1,17 +1,17 @@
 const cucumber = {
-    temperature: 0
+  temperature: 0
 };
 
 const potato = {
-    temperature: 100
+  temperature: 100
 };
 
 const makeSomethingCoolAsACucumber = thing => {
-    thing['temperature'] = cucumber.temperature;
-    return thing;
+  thing['temperature'] = cucumber.temperature;
+  return thing;
 }
 
 const makeSomethingHotAsAPotato = thing => {
-    thing['temperature'] = potato.temperature;
-    return thing;
+  thing['temperature'] = potato.temperature;
+  return thing;
 }
