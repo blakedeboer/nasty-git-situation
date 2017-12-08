6c013502c11284c7886241fac87ce0619b3dbfe5
Blake DeBoer <blakedeboer@users.noreply.github.com>
2017-12-08 10:02:56 -0500

    Remove merge conflict

--- tempChanger.js
+++ tempChanger.js
@@ -7,23 +7,13 @@ const potato = {
 };
 
 const makeSomethingCoolAsACucumber = thing => {
-<<<<<<< HEAD
-  thing['temperature'] = cucumber.temperature;
-  return thing;
+  const something = { ...thing };
+  something['temperature'] = cucumber.temperature;
+  return something;
 }
 
 const makeSomethingHotAsAPotato = thing => {
-  thing['temperature'] = potato.temperature;
-  return thing;
-=======
-    const something = { ...thing };
-    something['temperature'] = cucumber.temperature;
-    return something;
-}
-
-const makeSomethingHotAsAPotato = thing => {
-    const something = { ...thing };
-    something['temperature'] = potato.temperature;
-    return something;
->>>>>>> Prevent mutation of original object
+  const something = { ...thing };
+  something['temperature'] = potato.temperature;
+  return something;
 }
